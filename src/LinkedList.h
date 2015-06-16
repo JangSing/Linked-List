17673fec0459f5681296026195799514723b0e3f
JangSing <jangsing94@live.com.my>
2015-06-16 16:31:27 +0800

    Linked List Assignment

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -1,18 +1,12 @@
 #ifndef LinkedList_H
 #define LinkedList_H
 
-
-
-
 struct LinkedList{
 	struct ListElement *head;
 	struct ListElement *tail;
 };
 
 struct LinkedList *createLinkedList();
-
-
-
 
 
 #endif // LinkedList_H
